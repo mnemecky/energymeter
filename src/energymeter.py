5bce377ab0981dee95337048fd3af42e837eae22
Michael Nemecky <michael.nemecky@gmail.com>
2022-09-28 23:18:51 +0200

    fixed topic definitions

--- src/energymeter.py
+++ src/energymeter.py
@@ -11,20 +11,19 @@ sys.path.append("..")
 # Read env variables
 delay = int(os.getenv("DELAY")) or 300
 mqtt_host = os.getenv("MQTT_HOST") or "localhost"
-mqtt_topic = os.getenv("MQTT_TOPIC") or "/meter"
+mqtt_topic_root = os.getenv("MQTT_TOPIC") or "/meter"
 mqtt_client = os.getenv("MQTT_CLIENT") or "energymeter"
 mqtt_lwt = os.getenv("MQTT_LWT") or "lwt"
 debug = os.getenv("DEBUG").lower() == "on" or os.getenv("DEBUG").lower() == "true"
 
 # MQTT topics
-topic = mqtt_topic + '/' + mqtt_client
+topic = mqtt_topic_root + '/' + mqtt_client
 topic_power = topic + '/power'
 topic_counter = topic + '/counter'
 topic_cmd = topic + '/cmd'
 topic_lwt = topic + '/lwt'
 
 # MQTT commands
-CMD_RESET = 'reset'
 CMD_READENERGY = 'energy'
 
 
@@ -92,9 +91,7 @@ def cb_mqtt_connect(client, userdata, flags, rc):
 # callback on message
 def cb_mqtt_message(client, userdata, message):
     command = message.payload.decode("utf-8")
-    if command == CMD_RESET:
-        resetCounter()
-    elif command == CMD_READENERGY:
+    if command == CMD_READENERGY:
         energy = readEnergy()
 
 # connect to MQTT server
@@ -116,9 +113,10 @@ def mqtt_connect():
     return client
 
 
-def readEnergy(device):
+def readEnergy():
     global digiCounter
     global lastRead
+    global device
 
     # read counter from Digistump
     result = digi_readCounter(device)
@@ -129,7 +127,7 @@ def readEnergy(device):
     # the meter produces 1000 impulses per kWh
     power = ( result - digiCounter ) * ( 3600 / deltaT )
     if( power < 0 ):
-        digiCounter = resetCounter(device)
+        digiCounter = digi_resetCounter(device)
         lastRead = time.time()
     else:
         if debug: print('Power usage %d W, measurement period %d s' % (power, deltaT) )
@@ -139,26 +137,23 @@ def readEnergy(device):
     return power
 
 
-if __name__ == "__main__":
+# connect to Digistump device, reset counter
+device = digi_connectDevice()
+count = digi_resetCounter(device)
+lastRead = time.time()
+if debug: print('connected to Digistump device')
 
-    # connect to Digistump device, reset counter
-    device = digi_connectDevice()
-    count = digi_resetCounter(device)
-    lastRead = time.time()
-    if debug: print('connected to Digistump device')
+# connect to MQTT broker
+mqtt_conn = mqtt_connect()
 
-    # connect to MQTT broker
-    mqtt_conn = mqtt_connect()
+# main loop
+while 1 == 1:
+    time.sleep( delay )
 
-    # main loop
-    while 1 == 1:
+    # read counter from Digistump
+    power = readEnergy(device)
+    mqtt_conn.publish(topic_power, power )
 
-        time.sleep( delay )
-
-        # read counter from Digistump
-        power = readEnergy(device)
-        mqtt_conn.publish(mqtt_topic + '/power', power )
-
-    mqtt_conn.publish(topic_lwt,"OFF",0,True)
-    mqtt_conn.loop_stop()
-    mqtt_conn.disconnect()
+mqtt_conn.publish(topic_lwt,"OFF",0,True)
+mqtt_conn.loop_stop()
+mqtt_conn.disconnect()
